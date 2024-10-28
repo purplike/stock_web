f7e218df0802bf6f69d4df8c1b937a55dfc19a63
voidname <voidpurplesky@gmail.com>
2024-10-28 16:27:15 +0900

    사용쿼리

--- create_table.sql
+++ create_table.sql
@@ -4,3 +4,28 @@ CREATE TABLE Product (
      product_barcode VARCHAR2(100),
      stock_quantity NUMBER NOT NULL
 );
+
+create sequence product_seq;
+
+delete from product;
+
+select * from product;
+
+
+insert into product
+values
+    (
+      product_seq.nextval
+    , 'product 1'
+    , '0001'
+    , 0
+    );
+
+insert into product
+values
+    (
+      product_seq.nextval
+    , 'product 2'
+    , '0002'
+    , 0
+    );
